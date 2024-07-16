1645b42d388c9fd89393d6fe3130f67719db36c4
Kirill Martynov <kir.mart.vich@gmail.com>
2024-07-17 01:33:39 +0300

    Add redux, history, react-redux-history packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { HistoryRouter as Router } from 'redux-first-history/rr6';
+
+import { history, store } from '@core/redux/store.ts';
 
 import App from './App.tsx';
 
@@ -10,8 +13,10 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <Router history={history}>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
 );
