15043f267a34297ce33d774691bd355216fdb764
Kirill Martynov <kir.mart.vich@gmail.com>
2024-07-17 13:17:59 +0300

    Update Button component

--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -1,14 +1,30 @@
-import s from './Button.module.scss';
+import cn from 'classnames';
+
+import s from './Button.module.css';
 
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  className?: string;
+  theme?: 'primary' | 'secondary';
+  disabled?: boolean;
+  onClick?: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
-  const { children, ...otherProps } = props;
+  const { children, className, theme = 'primary', disabled, onClick, ...otherProps } = props;
+
+  const handleClick = () => {
+    onClick?.();
+  };
 
   return (
-    <button className={s.root} {...otherProps}>
+    <button
+      className={cn(s.root, s[theme], className, {
+        [s.disabled]: disabled,
+      })}
+      onClick={handleClick}
+      {...otherProps}
+    >
       {children}
     </button>
   );
